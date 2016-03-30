a0bf90841fa408d54f01ac728841b470882b7bd7
npgeorgiou <npgeorgiou@yahoo.gr>
2016-03-30 04:14:21 +0200

    fix(public/app/views): Seacargo is live again

--- server/app.js
+++ server/app.js
@@ -5,18 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
-var expressJwt = require('express-jwt');
 
 var app = express();
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.locals.pretty = true;
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -34,7 +30,6 @@ app.use(function (req, res, next) {
   next(err);
 });
 
-// error handlers
 
 // development error handler
 // will print stacktrace
