ee11dd6ee4de4ff93515a6612f4000656d96660d
Nikos Papageorgiou <npgeorgiou@yahoo.gr>
2017-03-06 18:03:14 +0100

    cleanup

--- server/app.js
+++ server/app.js
@@ -4,23 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var routes = require('./routes/index');
+var routes = require('./routes');
+
+global.__baseDir = path.resolve(__dirname, '..') + '/';
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.locals.pretty = true;
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.static(path.join(__dirname, '../public/app')));
-app.use(express.static(path.join(__dirname, '../public/img')));
-
 app.use('/', routes);
 
 // catch 404 and forward to error handler
@@ -30,28 +22,9 @@ app.use(function (req, res, next) {
   next(err);
 });
 
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.sendFile(path.join(__baseDir + '/public/views/404.html'))
 });
-
 
 module.exports = app;
---
+++ server/routes.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var jwt = require('jsonwebtoken');
+var http = require('http');
+var path = require('path');
+
+router.get('/nikos.papageorgiou', function (req, res) {
+    res.sendFile(path.join(__baseDir + '/public/views/nikos.papageorgiou/page.html'))
+});
+
+router.get('/', function (req, res) {
+    res.redirect("app/views/main.html")
+});
+
+module.exports = router;
